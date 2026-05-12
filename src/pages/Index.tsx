b64dbbc76c7aeb9c98faeb531fe92da10357691a
caralovell <168685711+caralovell@users.noreply.github.com>
2026-05-12 12:43:23 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,7 @@
 import HeroSection from "@/components/HeroSection";
 import DigitalSignageSection from "@/components/DigitalSignageSection";
 import SmartSignageSection from "@/components/SmartSignageSection";
-import WayfindingSection from "@/components/WayfindingSection";
-import IoTSection from "@/components/IoTSection";
 import AiAgentSection from "@/components/AiAgentSection";
-import ImmersiveSection from "@/components/ImmersiveSection";
 import BudgetSection from "@/components/BudgetSection";
 import FooterSection from "@/components/FooterSection";
 import SectionDivider from "@/components/SectionDivider";
@@ -17,13 +14,7 @@ const Index = () => (
     <SectionDivider />
     <SmartSignageSection />
     <SectionDivider />
-    <WayfindingSection />
-    <SectionDivider />
-    <IoTSection />
-    <SectionDivider />
     <AiAgentSection />
-    <SectionDivider />
-    <ImmersiveSection />
     <BudgetSection />
     <FooterSection />
   </div>
